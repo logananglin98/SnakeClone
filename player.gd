dab034ae5ce162f064e9a5593afeca458783eac8
logananglin98 <logananglin98@gmail.com>
2026-05-06 18:01:31 -0400

    Added player sprite. Created basic movement script

---
+++ player.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@export var speed = 200
+var direction = "right"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("left"):
+		direction = "left"
+	if Input.is_action_pressed("right"):
+		direction = "right"
+	if Input.is_action_pressed("up"):
+		direction = "up"
+	if Input.is_action_pressed("down"):
+		direction = "down"
+	
+	if direction == "left":
+		velocity = Vector2(-1, 0)
+	elif direction == "right":
+		velocity = Vector2(1, 0)
+	elif direction == "up":
+		velocity = Vector2(0, -1)
+	elif direction == "down":
+		velocity = Vector2(0, 1)
+		
+	velocity = velocity * speed
+	
+	position += velocity * delta
+		
